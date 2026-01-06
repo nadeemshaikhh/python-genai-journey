c6e9cfae7c31647cc5b2b91290949bf9c12de0e6
nadeemshaikhh <138777277+nadeemshaikhh@users.noreply.github.com>
2026-01-06 14:45:56 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+name = "nadeem"
+skills = "JavaScript"
+learning = "Python for GenAI"
+
+print("Name:", name)
+print("Skills:", skills)
+print("Currently learning:", learning)
+
+age = 22
+goal = "Remote GenAI Developer"
+
+print("Age:" , age)
+print("Goal:", goal)
+
+if age >= 18:
+  print("Status: Adult, eligible to work")
+else:
+  print("Status: Minor")
