37b1cf3a1bfbb4ae788cb593414dc225c47bf715
nadeemshaikhh <138777277+nadeemshaikhh@users.noreply.github.com>
2026-01-12 03:14:24 +0530

    Update day6_rock_paper_scissors.py

--- day6_rock_paper_scissors.py
+++ day6_rock_paper_scissors.py
@@ -2,22 +2,23 @@ import random
 
 choices = ["rock","paper","scissors"]
 
-user = input("Choose rock, paper, scissors: ").lower()
-if user not in choices:
-  print("Invalid choice! Please run the game again and choose correctly.")
-  exit()
-computer = random.choice(choices)
+while True:
+  user = input("Choose rock, paper, scissors: ").lower()
+  if user not in choices:
+      print("Invalid choice! Please run the game again and choose correctly.")
+      continue
+  computer = random.choice(choices)
 
-print("You chose:", user)
-print("Computer chose:", computer)
+  print("You chose:", user)
+  print("Computer chose:", computer)
 
-if user == computer:
-  print("It's a tie")
-elif user == "rock" and computer == "scissors":
-  print("You win!")
-elif user == "paper" and computer == "rock":
-  print("You win!")
-elif user == "scissors" and computer == "paper":
-  print("You win!")
-else:
-  print("You lose!")
+  if user == computer:
+    print("It's a tie")
+  elif user == "rock" and computer == "scissors":
+    print("You win!")
+  elif user == "paper" and computer == "rock":
+    print("You win!")
+  elif user == "scissors" and computer == "paper":
+    print("You win!")
+  else:
+    print("You lose!")
